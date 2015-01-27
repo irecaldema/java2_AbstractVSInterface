1861c2a68b2d73ac5b480842c4ae5b6b270b0187
zubiri <irecaldema14dw@ikzubirimanteo.com>
2015-01-27 07:30:58 +0000

    a vs i

--- src/Mesa.java
+++ src/Mesa.java
@@ -1,13 +1,33 @@
 public class Mueble extends Mueble implements Precio{
 	
+    int [] medidas;	
+    String montaje;
 
-    public Mueble (String modelo,int [] medidas, int n_piezas){
+    public Mueble (String modelo, int n_piezas,int [] medidas){
     	super.(modelo,medidas,n_piezas);
+    	this.medidas=medidas;
     }
     
-	public double precioconiva() {
-		precioIva = precio+precio*iva/100;
-		return Math.round(precioIva);
+    public setMontaje (montaje){
+    	this montaje=montaje;
+    }
+    
+    public string getMontaje (){
+    	return montaje;
+    }
+    
+	public double precioconiva(precio) {
+		p_iva = precio+precio*iva/100;
+		return Math.round(p_iva);
+	}
+	public double descuento(rebaja){
+		descuento=p_iva*rebaja;
+		return Math.round(descuento);
+	}
+	public double preciofinal(p_iva,descuento){
+		int p_final=0;
+		p_final=p_iva-descuento;
+		return p_final;
 	}
     
 }
--- src/Mueble.java
+++ src/Mueble.java
@@ -1,7 +1,20 @@
 public abstract Mueble{
     String modelo;
-    int medidas;
     int n_piezas;
+    int stock;
     
+    public mueble(String modelo, int n_piezas, int stock){
+        this.modelo=modelo;
+        this.n_piezas=n_piezas;
+        this.stock=stock;
+    }
+    
+    public existencias(stock){
+        if (stock>0){
+            return true;
+        }
+    }
+    
+    public montaje();
     
 }
